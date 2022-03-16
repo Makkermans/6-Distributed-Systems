fbc95d3a4b8d92ee0f08d2115cf6da68e25bda1f
iliB <56929828+bobmeemee@users.noreply.github.com>
2022-03-16 16:32:14 +0100

    copy paste van site

---
+++ src/Server.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String greeting = in.readLine();
+        if ("hello server".equals(greeting)) {
+            out.println("hello client");
+        }
+        else {
+            out.println("unrecognised greeting");
+        }
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+    public static void main(String[] args) throws IOException {
+        Server server=new Server();
+        server.start(6666);
+    }
+}
