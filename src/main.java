559195b7b89ea5d1dac8f564f61e2281378809a5
ilias <ilibtwee@gmail.com>
2022-03-16 12:20:56 +0100

    help

---
+++ src/main.java
@@ -0,0 +1,13 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        Main main = new Main();
+        main.run();
+    }
+
+    public void run()
+    {
+        System.out.println("hello world");
+    }
+}
